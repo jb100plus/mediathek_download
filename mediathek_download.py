b5a13e02394574c3c9e988341c7d856c06f089cd
jb100plus <46630606+jb100plus@users.noreply.github.com>
2019-01-12 18:34:29 +0100

    initial commit

---
+++ mediathek_download.py
@@ -0,0 +1,32 @@
+import urllib.request
+import sys
+import os.path
+
+
+if len(sys.argv) != 3:
+    print('usage mediathek_download.py source_url destination_file')
+    print('the source must contain a placeholder for the segment number (curly braces)')
+    print('e.g. https://mediathek/segment{}_123_av.ts')
+    exit(0)
+
+source = sys.argv[1]
+destination_filename = sys.argv[2]
+
+if os.path.isfile(destination_filename):
+    print('file {0} exists, aborting.'.format(destination_filename))
+    exit(0)
+
+destination = open(destination_filename, 'wb')
+
+for i in range(1, 2000):
+    try:
+        nurl = source.format(i)
+        videoreqest = urllib.request.urlopen(nurl)
+        segmentdata = videoreqest.read()
+        destination.write(segmentdata)
+    except urllib.error.HTTPError as he:
+        print('download finished {} segments'.format(i - 1))
+        break
+
+destination.close()
+
